178befc23c328323e096638ccce086be81232ef2
Darshan Pagar <mrdarshanpagar@gmail.com>
2022-09-19 22:35:19 +0530

    union of two sorted arrays

---
+++ union.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    //  union of two sorted arrays
+
+    vector<int> v1= {1,1,1,2,2,3,3,3};
+    vector<int> v2 = {3,3,3,4};
+    
+    vector<int> ans;
+    int left = 0, right = 0;
+
+    while(left < v1.size() || right < v2.size()){
+
+        // skip duplicate
+
+        while(left>0 && left<v1.size() && v1[left] == v1[left-1]){
+            left++;
+        }
+
+        while(right>0 && right<v2.size() && v2[right] == v2[right-1]){
+            right++;
+        }
+
+        // one array exhausted
+        if(left >= v1.size()){
+            ans.push_back(v2[right]);
+            right++;
+            continue;
+        }
+
+        if(right >= v2.size()){
+            ans.push_back(v2[left]);
+            left++;
+            continue;
+        }
+
+        // comparison
+
+        if(v1[left] < v2[right]){
+            ans.push_back(v1[left]);
+            left++;
+        }
+        else if(v1[left] > v2[right]){
+            ans.push_back(v2[right]);
+            right++;
+        }
+        else{
+            ans.push_back(v1[left]);
+            left++;
+            right++;
+        }
+    }
+
+    for(int i : ans){
+        cout<<i<<" ";
+    }
+
+    
+    return 0;
+}
+
